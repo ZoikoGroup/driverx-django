3932456aa4a970ab8052b4189d4ce7637658c20d
DeyDebasish2003 <04debasish03@gmail.com>
2026-03-20 19:47:18 +0530

    Revert "enabled send message"
    This reverts commit ea0b2a45f050fb42ae580f5ff96b3c9e962868a6.

--- apps/contact/serializers.py
+++
@@ -1,9 +0,0 @@
-# apps/contact/serializers.py
-
-from rest_framework import serializers
-from .models import ContactMessage
-
-class ContactMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactMessage
-        fields = '__all__'
